5cb8e67df2fe56b436cef6d46b2d5b0eb0e3b8da
K.M Ahnaf Zamil <ahnaf@ahnafzamil.com>
2022-10-03 21:04:39 +0600

    Changed screenshot

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -6,8 +6,8 @@
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int default_border = 0;   /* to switch back to default border after dynamic border resizing via keybinds */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const unsigned int cornerrad = 20;
